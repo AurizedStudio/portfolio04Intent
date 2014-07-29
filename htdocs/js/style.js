6bbdb742aa493697644a7fabe311e0cadbf6d825
T.Suzaki <info@aurized-studio.jp>
2014-07-29 18:44:48 +0900

    #24 ボタンのクリックテスト練習コードに注釈を付ける

--- htdocs/js/style.js
+++ htdocs/js/style.js
@@ -3,14 +3,14 @@ $(function(){
     $btnClick.click(function(){
 //        e.stopPropagation();
         alert("click(fn)");
-        return false;
+        return false; // リンクの遷移+イベント伝播 をキャンセル　e.preventDefault(); でリンクの遷移をキャンセル
     });
 });
 $(function(){
     var $btn = $(".btn-a");
 //    var btnon = $(".is-btn-a-on");
     $btn.on('click', '.is-btn-a-on', function(e){
-        e.stopPropagation();
+        e.stopPropagation(); // イベント伝播をキャンセル
         alert('on click selector');
     });
 });
