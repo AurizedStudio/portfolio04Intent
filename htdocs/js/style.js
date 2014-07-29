ecc0e2877c12cbb185ba854cff3ec75bd8cf073b
T.Suzaki <info@aurized-studio.jp>
2014-07-29 19:48:06 +0900

    #24 変数割り当て方

--- htdocs/js/style.js
+++ htdocs/js/style.js
@@ -1,3 +1,4 @@
+// ボタンクリック
 $(function(){
     var $btnClick = $(".is-btn-a-click");
     $btnClick.click(function(){
@@ -5,12 +6,18 @@ $(function(){
         alert("click(fn)");
         return false; // リンクの遷移+イベント伝播 をキャンセル　e.preventDefault(); でリンクの遷移をキャンセル
     });
-});
-$(function(){
     var $btn = $(".btn-a");
 //    var btnon = $(".is-btn-a-on");
     $btn.on('click', '.is-btn-a-on', function(e){
         e.stopPropagation(); // イベント伝播をキャンセル
         alert('on click selector');
     });
+});
+
+// ボタン 変数
+$(function(){
+    var btnB = $('.btn-b'); // $btnB としなくよい
+    btnB.find($('.is-btn-b-default')).on('click',  function(){
+            $(this).parent().next().children().toggleClass('is-btn-b-appear');
+        });
 });
