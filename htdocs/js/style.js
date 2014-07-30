52a58c019c272615ec671c7474d691835b0ddcac
AurizedStudio <info@aurized-studio.jp>
2014-07-30 09:44:48 +0900

    #24 チェックボックスにチェックされたら送信可能にするボタン作成 Gruntはproxyを変更

--- htdocs/js/style.js
+++ htdocs/js/style.js
@@ -21,3 +21,17 @@ $(function(){
             $(this).parent().next().children().toggleClass('is-btn-b-appear');
         });
 });
+
+// チェックボックス/サブミットボタン
+$(function(){
+	var btnSubmit = $('#btn-c-submit');
+	var btnCheck = $('#btn-c-check-agree');
+	btnSubmit.prop('disabled', true); // 初期submitボタンをoffに
+	btnCheck.on('click', function(){ // チェックボックスをクリックする
+		if($(this).prop('checked') == false){
+			btnSubmit.prop('disabled', true);
+		} else {
+			btnSubmit.prop('disabled', false);
+		}
+	});
+});
